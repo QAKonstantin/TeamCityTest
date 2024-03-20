b8b3a7e38651bf66789761a9101a89afa49346f9
Konstantin <k.korepanov@etpgpb.ru>
2024-03-20 20:20:53 +0300

    fix tests

--- pages/build_conf_page.py
+++ pages/build_conf_page.py
@@ -28,7 +28,7 @@ class RunBuildPage(BasePage):
         last_build_status = BuildStatus.BUILD_UNKNOWN.value
         timeout = 0
         while last_build_status != expected_status:
-            if timeout != 10:
+            if timeout != 180:
                 time.sleep(1)
                 last_build_status = self.overview.get_all_running_builds().first.inner_text().upper()
                 timeout += 1

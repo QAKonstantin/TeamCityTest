49c5ce9dcb451d15c84cec872536387431117062
Konstantin <k.korepanov@etpgpb.ru>
2024-03-26 20:34:37 +0300

    fix allure attachment

--- actions/page_actions.py
+++ actions/page_actions.py
@@ -11,18 +11,23 @@ class PageAction:
             self.page.goto(url)
 
     def check_url(self, expected_url: str, timeout=10000):
-        with allure.step(f"Проверка URL: ожидаемый URL - {expected_url}"):
-            expect(self.page).to_have_url(expected_url, timeout=timeout)
+        try:
+            with allure.step(f"Проверка URL: ожидаемый URL - {expected_url}"):
+                expect(self.page).to_have_url(expected_url, timeout=timeout)
+        except TimeoutError:
+            self.allure_attach()
+            raise AssertionError(f'URL должен быть равен {expected_url}')
 
+    @allure.step("Проверка, что ссылка открылась в новой вкладке")
     def check_open_in_new_tab(self, expected_url: str, timeout: int = 90000):
-        with allure.step(f"Проверка, что ссылка открылась в новой вкладке"):
-            try:
-                with self.page.context.expect_page(timeout=timeout) as new_page:
-                    expect(new_page.value).to_have_url(expected_url, timeout=timeout)
-            except Exception:
-                raise AssertionError(
-                    f"Некорректная ссылка: {new_page.value} \n ОР: {expected_url} \n"
-                    f"или ссылка должна открываться в новой вкладке")
+        try:
+            with self.page.context.expect_page(timeout=timeout) as new_page:
+                expect(new_page.value).to_have_url(expected_url, timeout=timeout)
+        except Exception:
+            self.allure_attach()
+            raise AssertionError(
+                f"Некорректная ссылка: {new_page.value} \n ОР: {expected_url} \n"
+                f"или ссылка должна открываться в новой вкладке")
 
     def allure_attach(self):
         allure.attach(
@@ -31,21 +36,29 @@ class PageAction:
             attachment_type=allure.attachment_type.PNG
         )
 
+    @allure.step(f"Закрыть вкладку")
     def close_tab(self, n=-1):
         """
         :param n: Номер вкладки, которую необходимо закрыть. По умолчанию закрывается последняя вкладка
         """
-        with allure.step(f"Закрыть вкладку"):
-            all_tabs = self.page.context.pages
-            all_tabs[n].close()
+        all_tabs = self.page.context.pages
+        all_tabs[n].close()
 
     def wait_for_url_changed(self, expected_url: str, timeout: int = 60000):
-        with allure.step(f"Ожидание изменения URL на {expected_url}"):
-            self.page.wait_for_url(expected_url, timeout=timeout)
+        try:
+            with allure.step(f"Ожидание изменения URL на {expected_url}"):
+                self.page.wait_for_url(expected_url, timeout=timeout)
+        except TimeoutError:
+            self.allure_attach()
+            raise AssertionError(f'Ссылка {self.page.url} не изменилась на URL {expected_url}')
 
+    @allure.step("Ожидание загрузки страницы")
     def wait_for_page_load(self):
-        with allure.step(f"Ожидание загрузки страницы"):
+        try:
             self.page.wait_for_load_state('load')
+        except TimeoutError:
+            self.allure_attach()
+            raise AssertionError('Страница не загрузилась полностью')
 
     def click_button(self, selector, timeout=60000):
         try:
@@ -82,8 +95,12 @@ class PageAction:
             raise AssertionError(f'Элемент {selector} не найден')
 
     def get_text(self, selector) -> str:
-        with allure.step(f"Получить текст по локатору: {selector}"):
-            return self.page.locator(selector).inner_html()
+        try:
+            with allure.step(f"Получить текст по локатору: {selector}"):
+                return self.page.locator(selector).inner_html()
+        except TimeoutError:
+            self.allure_attach()
+            raise AssertionError(f'Тест не был получен по локатору: {selector}')
 
     def input_text(self, selector, text):
         with allure.step(f"Ввод текста {text} в элемент: {selector}"):
@@ -110,20 +127,32 @@ class PageAction:
             raise AssertionError(f'Элемент {selector} не исчез')
 
     def assert_text_present_on_page(self, text):
-        with allure.step(f"Проверка наличия текста '{text}' на странице"):
-            expect(self.page).to_have_text(text)
+        try:
+            with allure.step(f"Проверка наличия текста '{text}' на странице"):
+                expect(self.page).to_have_text(text)
+        except TimeoutError:
+            self.allure_attach()
+            raise AssertionError(f'Текст {text} отсутствует на странице')
 
     def assert_text_in_element(self, selector, text):
-        with allure.step(f"Проверка наличия текста '{text}' в элементе: {selector}"):
-            expect(self.page.locator(selector)).to_have_text()
+        try:
+            with allure.step(f"Проверка наличия текста '{text}' в элементе: {selector}"):
+                expect(self.page.locator(selector)).to_have_text()
+        except TimeoutError:
+            self.allure_attach()
+            raise AssertionError(f'Текст {text} отсутствует в элементе: {selector}')
 
     def assert_element_attribute(self, selector, attribute, value):
         with allure.step(f"Проверка значения {value} атрибута {attribute} элемента {selector}"):
             expect(self.page.locator(selector)).to_have_attribute(attribute, value)
 
     def assert_element_hidden(self, selector):
-        with allure.step(f"Проверка, что элемент {selector} скрыт"):
-            expect(self.page.locator(selector)).to_be_hidden()
+        try:
+            with allure.step(f"Проверка, что элемент {selector} скрыт"):
+                expect(self.page.locator(selector)).to_be_hidden()
+        except TimeoutError:
+            self.allure_attach()
+            raise AssertionError(f'Элемент {selector} отображается, хотя должен быть скрыт')
 
     def assert_count_elements(self, selector, n: int):
         with allure.step(f"Проверка на количество элементов {selector}"):

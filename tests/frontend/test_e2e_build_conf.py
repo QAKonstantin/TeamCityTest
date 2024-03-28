2f520e42c750d08cc9f4f4c93864b1fbd80fe9e7
Konstantin <k.korepanov@etpgpb.ru>
2024-03-28 17:12:13 +0300

    add classes

--- tests/frontend/test_e2e_build_conf.py
+++ tests/frontend/test_e2e_build_conf.py
@@ -8,87 +8,90 @@ from pages.create_build_conf_page import BuildConfCreationPage
 from pages.edit_build_conf_page import EditBuildPage
 
 
-@allure.severity(allure.severity_level.CRITICAL)
-@allure.parent_suite("UI тесты")
-@allure.suite("Управление билд конфигурациями")
-@allure.sub_suite("Cоздание билд конфигурации")
-@allure.title("Cоздание билд конфигурации и шага в билде Супер Администратором")
-@allure.description("Авторизация Супер Администратором, создание проекта, билд конфигурации и шага в командной строке")
-@pytest.mark.build_conf
-@pytest.mark.ui
-def test_e2e_create_build_conf(project_data, build_conf_data, random_description, browser, super_admin):
-    with allure.step('Подготовка данных для создания проекта и билд конфигурации'):
-        project_data_1 = project_data()
-        build_conf_data_1 = build_conf_data(project_data_1.id)
-        custom_script = build_conf_data_1.steps['step'][0]['properties']['property'][0]['value']
+class TestBuildConfE2E:
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.parent_suite("UI тесты")
+    @allure.suite("Управление билд конфигурациями")
+    @allure.sub_suite("Cоздание билд конфигурации")
+    @allure.title("Cоздание билд конфигурации и шага в билде Супер Администратором")
+    @allure.description(
+        "Авторизация Супер Администратором, создание проекта, билд конфигурации и шага в командной строке")
+    @pytest.mark.build_conf
+    @pytest.mark.ui
+    def test_e2e_create_build_conf(self, project_data, build_conf_data, random_description, browser, super_admin):
+        with allure.step('Подготовка данных для создания проекта и билд конфигурации'):
+            project_data_1 = project_data()
+            build_conf_data_1 = build_conf_data(project_data_1.id)
+            custom_script = build_conf_data_1.steps['step'][0]['properties']['property'][0]['value']
 
-    with allure.step('Отправка запроса на создание проекта'):
-        created_project_response = super_admin.api_manager.project_api.create_project(
-            project_data_1.model_dump()).text
-        created_project_model = ProjectResponseModel.model_validate_json(created_project_response)
-        assert created_project_model.id == project_data_1.id, \
-            f"Ожидался project_id= {project_data_1.id}, но был получен {created_project_model.id}"
+        with allure.step('Отправка запроса на создание проекта'):
+            created_project_response = super_admin.api_manager.project_api.create_project(
+                project_data_1.model_dump()).text
+            created_project_model = ProjectResponseModel.model_validate_json(created_project_response)
+            assert created_project_model.id == project_data_1.id, \
+                f"Ожидался project_id= {project_data_1.id}, но был получен {created_project_model.id}"
 
-    with allure.step("Авторизация под Супер Администратором"):
-        login_browser = AuthLoginBySuperAdmin(browser)
-        login_browser.login_by_super_admin()
+        with allure.step("Авторизация под Супер Администратором"):
+            login_browser = AuthLoginBySuperAdmin(browser)
+            login_browser.login_by_super_admin()
 
-    with allure.step("Создание билд конфигурации"):
-        build_conf_creation_browser = BuildConfCreationPage(browser, project_data_1.id)
-        build_conf_creation_browser.create_build_conf(project_data_1.id, build_conf_data_1.name, build_conf_data_1.id,
-                                                      random_description)
+        with allure.step("Создание билд конфигурации"):
+            build_conf_creation_browser = BuildConfCreationPage(browser, project_data_1.id)
+            build_conf_creation_browser.create_build_conf(project_data_1.id, build_conf_data_1.name,
+                                                          build_conf_data_1.id,
+                                                          random_description)
 
-    with allure.step("Добавление шага"):
-        build_step_creation_browser = EditBuildPage(browser)
-        build_step_creation_browser.create_build_step(build_conf_data_1.name, custom_script,
-                                                      build_conf_data_1.id)
+        with allure.step("Добавление шага"):
+            build_step_creation_browser = EditBuildPage(browser)
+            build_step_creation_browser.create_build_step(build_conf_data_1.name, custom_script,
+                                                          build_conf_data_1.id)
 
-    with allure.step('Отправка запроса на получение информации по созданной билд конфигурации и наличию шага в билде'):
-        data_build_conf_response = super_admin.api_manager.build_conf_api.get_build_conf_by_locator(
-            build_conf_data_1.id).text
-        created_build_conf = BuildConfResponseModel.model_validate_json(data_build_conf_response)
-        assert created_build_conf.id == build_conf_data_1.id, \
-            f"Не найден созданный build_conf_id={created_build_conf.id} среди существующих билд конфигураций"
+        with allure.step(
+                'Отправка запроса на получение информации по созданной билд конфигурации и наличию шага в билде'):
+            data_build_conf_response = super_admin.api_manager.build_conf_api.get_build_conf_by_locator(
+                build_conf_data_1.id).text
+            created_build_conf = BuildConfResponseModel.model_validate_json(data_build_conf_response)
+            assert created_build_conf.id == build_conf_data_1.id, \
+                f"Не найден созданный build_conf_id={created_build_conf.id} среди существующих билд конфигураций"
 
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.parent_suite("UI тесты")
+    @allure.suite("Управление билд конфигурациями")
+    @allure.sub_suite("Удаление билд конфигурации")
+    @allure.title("Удаление билд конфигурации под Супер Администратором")
+    @allure.description("Авторизация Супер Администратором, создание проекта, билда и удаление билда")
+    @pytest.mark.build_conf
+    @pytest.mark.ui
+    def test_e2e_delete_build_conf(self, project_data, browser, super_admin):
+        with allure.step('Подготовка данных для создания проекта и билд конфигурации'):
+            project_data_1 = project_data()
+            build_conf_data_1 = BuildConfData.create_build_conf_data(project_data_1.id)
 
-@allure.severity(allure.severity_level.CRITICAL)
-@allure.parent_suite("UI тесты")
-@allure.suite("Управление билд конфигурациями")
-@allure.sub_suite("Удаление билд конфигурации")
-@allure.title("Удаление билд конфигурации под Супер Администратором")
-@allure.description("Авторизация Супер Администратором, создание проекта, билда и удаление билда")
-@pytest.mark.build_conf
-@pytest.mark.ui
-def test_e2e_delete_build_conf(project_data, browser, super_admin):
-    with allure.step('Подготовка данных для создания проекта и билд конфигурации'):
-        project_data_1 = project_data()
-        build_conf_data_1 = BuildConfData.create_build_conf_data(project_data_1.id)
+        with allure.step('Отправка запроса на создание проекта'):
+            created_project_response = super_admin.api_manager.project_api.create_project(
+                project_data_1.model_dump()).text
+            created_project_model = ProjectResponseModel.model_validate_json(created_project_response)
+            assert created_project_model.id == project_data_1.id, \
+                f"Ожидался project_id= {project_data_1.id}, но был получен {created_project_model.id}"
 
-    with allure.step('Отправка запроса на создание проекта'):
-        created_project_response = super_admin.api_manager.project_api.create_project(
-            project_data_1.model_dump()).text
-        created_project_model = ProjectResponseModel.model_validate_json(created_project_response)
-        assert created_project_model.id == project_data_1.id, \
-            f"Ожидался project_id= {project_data_1.id}, но был получен {created_project_model.id}"
+        with allure.step('Отправка запроса на создание билд конфигурации'):
+            create_build_conf_response = super_admin.api_manager.build_conf_api.create_build_conf(
+                build_conf_data_1.model_dump()).text
+            build_conf_model = BuildConfResponseModel.model_validate_json(create_build_conf_response)
+            with pytest.assume:
+                assert build_conf_model.id == build_conf_data_1.id, \
+                    f"Ожидался build_conf_id={build_conf_data_1.id}, но был получен {build_conf_model.id}"
 
-    with allure.step('Отправка запроса на создание билд конфигурации'):
-        create_build_conf_response = super_admin.api_manager.build_conf_api.create_build_conf(
-            build_conf_data_1.model_dump()).text
-        build_conf_model = BuildConfResponseModel.model_validate_json(create_build_conf_response)
-        with pytest.assume:
-            assert build_conf_model.id == build_conf_data_1.id, \
-                f"Ожидался build_conf_id={build_conf_data_1.id}, но был получен {build_conf_model.id}"
+        with allure.step("Авторизация под Супер Администратором"):
+            login_browser = AuthLoginBySuperAdmin(browser)
+            login_browser.login_by_super_admin()
 
-    with allure.step("Авторизация под Супер Администратором"):
-        login_browser = AuthLoginBySuperAdmin(browser)
-        login_browser.login_by_super_admin()
+        with allure.step("Удаление билд конфигурации"):
+            build_conf_edit_page_browser = EditBuildPage(browser)
+            build_conf_edit_page_browser.delete_build_conf(project_data_1.id, build_conf_data_1.id)
 
-    with allure.step("Удаление билд конфигурации"):
-        build_conf_edit_page_browser = EditBuildPage(browser)
-        build_conf_edit_page_browser.delete_build_conf(project_data_1.id, build_conf_data_1.id)
-
-    with (allure.step('Отправка запроса на проверку, что билд конфигурация действительно удалена')):
-        get_builds_response = super_admin.api_manager.build_conf_api.get_builds().json()
-        build_ids = [build.get('id', {}) for build in get_builds_response.get('buildType', [])]
-        assert build_conf_data_1.id not in build_ids, \
-            "ID созданной билд конфигурации найден в списке билдов после удаления"
+        with (allure.step('Отправка запроса на проверку, что билд конфигурация действительно удалена')):
+            get_builds_response = super_admin.api_manager.build_conf_api.get_builds().json()
+            build_ids = [build.get('id', {}) for build in get_builds_response.get('buildType', [])]
+            assert build_conf_data_1.id not in build_ids, \
+                "ID созданной билд конфигурации найден в списке билдов после удаления"
--- tests/frontend/test_e2e_run_build.py
+++ tests/frontend/test_e2e_run_build.py
@@ -7,42 +7,43 @@ from pages.auth_page import AuthLoginBySuperAdmin
 from pages.build_conf_page import RunBuildPage
 
 
-@allure.severity(allure.severity_level.CRITICAL)
-@allure.parent_suite("UI тесты")
-@allure.suite("Управление запусками билдов")
-@allure.sub_suite("Запуск билда")
-@allure.title("Cоздание проекта, билд конфигурации, шага и запуск билда")
-@allure.description(
-    "Авторизация Супер Администратором, создание проекта, билд конфигурации, шага в командной строке и запуск билда")
-@pytest.mark.build_run
-@pytest.mark.ui
-def test_e2e_run_build(project_data, build_conf_data, random_description, browser, super_admin):
-    with allure.step('Подготовка данных для создания проекта и билд конфигурации'):
-        project_data_1 = project_data()
-        build_conf_data_1 = build_conf_data(project_data_1.id)
+class TestBuildE2E:
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.parent_suite("UI тесты")
+    @allure.suite("Управление запусками билдов")
+    @allure.sub_suite("Запуск билда")
+    @allure.title("Cоздание проекта, билд конфигурации, шага и запуск билда")
+    @allure.description(
+        "Авторизация Супер Администратором, создание проекта, билд конфигурации, шага в командной строке и запуск билда")
+    @pytest.mark.build_run
+    @pytest.mark.ui
+    def test_e2e_run_build(self, project_data, build_conf_data, random_description, browser, super_admin):
+        with allure.step('Подготовка данных для создания проекта и билд конфигурации'):
+            project_data_1 = project_data()
+            build_conf_data_1 = build_conf_data(project_data_1.id)
 
-    with allure.step('Отправка запроса на создание проекта'):
-        created_project_response = super_admin.api_manager.project_api.create_project(
-            project_data_1.model_dump()).text
-        created_project_model = ProjectResponseModel.model_validate_json(created_project_response)
-        assert created_project_model.id == project_data_1.id, \
-            f"Ожидался project_id= {project_data_1.id}, но был получен {created_project_model.id}"
+        with allure.step('Отправка запроса на создание проекта'):
+            created_project_response = super_admin.api_manager.project_api.create_project(
+                project_data_1.model_dump()).text
+            created_project_model = ProjectResponseModel.model_validate_json(created_project_response)
+            assert created_project_model.id == project_data_1.id, \
+                f"Ожидался project_id= {project_data_1.id}, но был получен {created_project_model.id}"
 
-    with allure.step('Отправка запроса на создание билд конфигурации'):
-        create_build_conf_response = super_admin.api_manager.build_conf_api.create_build_conf(
-            build_conf_data_1.model_dump()).text
-        build_conf_model = BuildConfResponseModel.model_validate_json(create_build_conf_response)
-        with pytest.assume:
-            assert build_conf_model.id == build_conf_data_1.id, \
-                f"Ожидался build_conf_id={build_conf_data_1.id}, но был получен {build_conf_model.id}"
+        with allure.step('Отправка запроса на создание билд конфигурации'):
+            create_build_conf_response = super_admin.api_manager.build_conf_api.create_build_conf(
+                build_conf_data_1.model_dump()).text
+            build_conf_model = BuildConfResponseModel.model_validate_json(create_build_conf_response)
+            with pytest.assume:
+                assert build_conf_model.id == build_conf_data_1.id, \
+                    f"Ожидался build_conf_id={build_conf_data_1.id}, но был получен {build_conf_model.id}"
 
-    with allure.step("Авторизация под Супер Администратором"):
-        login_browser = AuthLoginBySuperAdmin(browser)
-        login_browser.login_by_super_admin()
+        with allure.step("Авторизация под Супер Администратором"):
+            login_browser = AuthLoginBySuperAdmin(browser)
+            login_browser.login_by_super_admin()
 
-    with allure.step("Запуск билда"):
-        run_build_browser = RunBuildPage(browser, build_conf_data_1.id)
-        run_build_browser.run_build()
+        with allure.step("Запуск билда"):
+            run_build_browser = RunBuildPage(browser, build_conf_data_1.id)
+            run_build_browser.run_build()
 
-    with allure.step("Проверка, что билд завершился успешно"):
-        run_build_browser.check_last_status_running_build()
+        with allure.step("Проверка, что билд завершился успешно"):
+            run_build_browser.check_last_status_running_build()
